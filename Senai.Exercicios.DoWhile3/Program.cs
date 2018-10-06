f5bd431b4ba1aee0f26a7ca760b67a2a1e6dcae0
Matheus Custódio <matheuscustodiosantos@hotmail.com>
2018-10-05 22:18:07 -0300

    Terminando exercicio do code dojo

--- Senai.Exercicios.DoWhile3/Program.cs
+++ Senai.Exercicios.DoWhile3/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Senai.Exercicios.DoWhile3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Senai.Exercicios.DoWhile3 {
+    class Program {
+        static void Main (string[] args) {
+            // int divisao = 0;
+            int contador = 0;
+            float divisor = 0;
+
+            Console.WriteLine ("Insire o valor do dividendo: ");
+            float dividendo = float.Parse (Console.ReadLine ());
+
+
+            while (divisor == 0) {
+                Console.WriteLine ("Insira um valor diferente de zero para o divisor: ");
+                divisor = float.Parse (Console.ReadLine ());
+            }
+
+            do {
+                dividendo -= divisor;
+                contador++;
+            } while (dividendo > 0);
+
+            if (dividendo < 0){
+                dividendo = dividendo - 1;
+            }
+            
+            Console.WriteLine ($"O resultado da divisão é: {contador}");
         }
     }
 }
