44e32efb137ee636e8220d50c798fb11a7629c53
Matheus Custodio Santos <44846245837@info.local>
2018-10-29 17:21:47 -0300

    Uns bug e techtalk

---
+++ Senai.Techtalk/Classes/Cadastro.cs
@@ -0,0 +1,8 @@
+namespace Senai.Techtalk.Classes
+{
+    public class Cadastro
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ Senai.Techtalk/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Senai.Techtalk.Classes;
+
+namespace Senai.Techtalk
+{
+    class Program
+    {
+        static Cadastro[] usuarios = new Cadastro[10];
+        static int qtdCadastros = 0;
+        static bool senha = false;
+        static bool email = false;
+
+        static void Main(string[] args)
+        {
+
+            StreamWriter writer = new StreamWriter(@"C:\Users\44846245837\Desktop\Csharp\Senai.Techtalk\teste.txt"); 
+            StreamReader reader = new StreamReader(@"C:\Users\44846245837\Desktop\Csharp\Senai.Techtalk\teste.txt");
+
+
+            usuarios[qtdCadastros] = new Cadastro();
+            if (qtdCadastros < usuarios.Length)
+            {
+                do
+                {
+                    System.Console.WriteLine("Insira o nome do usuário a ser cadastrado: ");
+                    usuarios[qtdCadastros].Nome = Console.ReadLine();
+                    foreach (var item in usuarios)
+                    {
+                        if (item != null)
+                        {
+                            if (item.Nome == reader.ReadLine())
+                            {
+                                System.Console.WriteLine("Esse email já existe!!");
+                            }
+                            else
+                            {
+                                email = true;
+                                break;
+                            }
+                        }
+                    }
+                } while (!email);
+
+                do
+                {
+                    System.Console.WriteLine("Insira a senha do usuário a ser cadastrado: ");
+                    usuarios[qtdCadastros].Senha = Console.ReadLine();
+                    foreach (var item in usuarios)
+                    {
+                        if (item != null)
+                        {
+                            if (item.Senha == reader.ReadLine())
+                            {
+                                System.Console.WriteLine("Essa senha já existe!!");
+                            }
+                            else
+                            {
+                                senha = true;
+                                break;
+                            }
+                        }
+                    }
+                } while (!senha);
+
+                writer.WriteLine(usuarios[qtdCadastros].Nome +":"+ usuarios[qtdCadastros].Senha);
+                qtdCadastros++;
+            }
+        }
+    }
+}
